202dc58f496219c5dbf420ae2da2a87d24b73fb4
Jayjack0116 <opudding7654321@gmail.com>
2015-10-12 23:11:50 +0800

    Iterative method with reading sorted string array

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,36 @@
 /* FIXME: Implement! */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char smallest_character(char str[], char c)
+{
+	int len = strlen(str);
+	for(int i = 0; i < len; i++){
+		if( str[i] > c)
+			return str[i];
+	}
+	return str[0];
+}
 
 int main()
 {
+	FILE *fp;
+	char buffer[1024];
+	int i = 0,c=0;
+	fp = fopen("str_array.txt","r");
+	if( fp == NULL)
+		printf("Open failure\n");
+	else{
+		for(c = fgetc(fp); c != EOF ; i++){
+			buffer[i] = c;
+			c = fgetc(fp);
+		}
+	}
+	fclose(fp);
+	char str[i];
+	strncpy(str,buffer,i);
+	char character = smallest_character(str,'d');
+	printf("%c\n", character);
     return 0;
 }
