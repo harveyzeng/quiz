df152075f971b2fad4a6ed4467e4aeb4c7cd8c4f
Jayjack0116 <opudding7654321@gmail.com>
2015-10-14 21:23:56 +0800

    Iterative flatten tree

--- q3/iterative.c
+++ q3/iterative.c
@@ -1,6 +1,46 @@
 /* FIXME: Implement! */
+#include <stdlib.h>
+#include <stdio.h>
+#include <tree.h>
+void flatten(struct TreeNode* root)
+{
+    struct TreeNode* leftt;
+    while(root!=NULL) {
+        if(root->left!=NULL) {
+            leftt=root->left;
+            while(leftt->right!=NULL) leftt=leftt->right;
+            leftt->right=root->right;
+            root->right=root->left;
+            root->left=NULL;
+        }
+        root=root->right;
+    }
+}
 
 int main()
 {
+    Node *root = NULL;
+    insert(&root, 1);
+    insert(&(root->left), 2);
+    insert(&(root->left->left), 3);
+    insert(&(root->left), 4);
+    insert(&root, 5);
+    insert(&root, 6);
+
+    print_preorder(root);
+    printf("\n");
+    print_inorder(root);
+    printf("\n");
+    print_postorder(root);
+    printf("\n");
+
+    flatten(root);
+
+    print_preorder(root);
+    printf("\n");
+    print_inorder(root);
+    printf("\n");
+    print_postorder(root);
+    printf("\n");
     return 0;
 }
