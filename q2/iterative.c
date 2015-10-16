1f8d5c663138f2371254f248ec3c8d3dbc40d998
harveyzeng <ff6416@hotmail.com>
2015-10-16 14:06:11 +0800

    add clock_gettime function to get the time for execute the recursive and iterative function

--- q2/iterative.c
+++ q2/iterative.c
@@ -2,11 +2,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <time.h>
+static double diff_in_second(struct timespec t1, struct timespec t2)
+{
+    struct timespec diff;
+    if (t2.tv_nsec-t1.tv_nsec < 0) {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
+    } else {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    }
+    return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
+}
 char smallest_character(char str[], char c)
 {
-	int len = strlen(str);
-	for(int i = 0; i < len; i++){
+	
+	for(int i = 0; i < strlen(str); i++){
 		if( str[i] > c)
 			return str[i];
 	}
@@ -15,23 +27,30 @@ char smallest_character(char str[], char c)
 
 int main()
 {
-	FILE *fp;
-	char buffer[1024];
-	int i = 0,c=0;
-	fp = fopen("str_array.txt","r");
-	if( fp == NULL)
-		printf("Open failure\n");
-	else{
-		for(c = fgetc(fp); c != EOF ; i++){
-			buffer[i] = c;
-			c = fgetc(fp);
-		}
-	}
-	fclose(fp);
-	char str[i];
-	strncpy(str,buffer,i);
-	char temp = 'a';
-	while(temp++ <= 'z')
-        smallest_character(str,temp);
+    FILE *fp;
+    struct timespec start, end;
+    char buffer[1024];
+    int i = 0,c=0;
+    double cpu_time1;
+    fp = fopen("str_array.txt","r");
+    if( fp == NULL)
+        printf("Open failure\n");
+    else {
+        for(c = fgetc(fp); c != EOF ; i++) {
+            buffer[i] = c;
+            c = fgetc(fp);
+        }
+    }
+    fclose(fp);
+    char str[i];
+    strncpy(str,buffer,i);
+    char temp = 'k';
+	clock_gettime(CLOCK_REALTIME, &start);
+	for(int j=0;j<1000;j++){
+        smallest_character(str,temp);}
+	clock_gettime(CLOCK_REALTIME, &end);
+    	cpu_time1 = diff_in_second(start, end);
+	printf("execution time of recursive() : %lf sec\n", cpu_time1);
     return 0;
 }
+
--- q2/recursive.c
+++ q2/recursive.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+static double diff_in_second(struct timespec t1, struct timespec t2)
+{
+    struct timespec diff;
+    if (t2.tv_nsec-t1.tv_nsec < 0) {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
+    } else {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    }
+    return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
+}
 
 char smallest_character(char str[], char c, int len)
 {
@@ -17,8 +30,10 @@ char smallest_character(char str[], char c, int len)
 int main()
 {
     FILE *fp;
+    struct timespec start, end;
     char buffer[1024];
     int i = 0,c=0;
+    double cpu_time1;
     fp = fopen("str_array.txt","r");
     if( fp == NULL)
         printf("Open failure\n");
@@ -32,8 +47,12 @@ int main()
     char str[i];
     strncpy(str,buffer,i);
     char temp = 'a';
-    while(temp++ <= 'z')
-        smallest_character(str,temp,i);
+	clock_gettime(CLOCK_REALTIME, &start);
+	for(int j=0;j<1000;j++){
+        smallest_character(str,temp,i);}
+	clock_gettime(CLOCK_REALTIME, &end);
+    	cpu_time1 = diff_in_second(start, end);
+	printf("execution time of recursive() : %lf sec\n", cpu_time1);
     return 0;
 }
 
